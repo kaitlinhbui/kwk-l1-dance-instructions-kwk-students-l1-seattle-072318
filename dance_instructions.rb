0b2b2619814dcbac954006b3806bddbd0c704855
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-24 16:45:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -26,3 +26,9 @@ def bob_the_reins
   puts "1. Holding your arms out in front of you, straight and at chest level"
   puts "2. Cross your right wrist over your left and hold them together"
   puts "3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times."
+end
+
+def lasso
+  puts "1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+   - Lift right arm up and point right elbow diagonally to the right.
+   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well."
