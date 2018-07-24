e58eedc8f31acc1367fe906247b42065a38b525f
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-24 16:42:39 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,26 +1,28 @@
 
 # Code your methods below
 def lower_body
-  put "1. Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
 end
 
 def base_footwork
-  put "1. Lift right foot"
-  put "2. Return right foot to the ground"
-  put "3. Finishing with a **small skip-step** backward"
+  puts "1. Lift right foot"
+  puts "2. Return right foot to the ground"
+  puts "3. Finishing with a **small skip-step** backward"
 end
 
 def skip_step_left
-  put "1. Lower left foot to the ground"
-  put "2. Bounce left foot back up slightly, kicking it a few inches back"
-  put "3. Left, Right, Left and Left"
+  puts "1. Lower left foot to the ground"
+  puts "2. Bounce left foot back up slightly, kicking it a few inches back"
+  puts "3. Left, Right, Left and Left"
 end
 
 def skip_step_right
-  put "1. Lower right foot to the ground"
-  put "2. Bounce right foot back up slightly, kicking it a few inches back"
-  put "3. Right, Left, Right and Right"
+  puts "1. Lower right foot to the ground"
+  puts "2. Bounce right foot back up slightly, kicking it a few inches back"
+  puts "3. Right, Left, Right and Right"
 end
 
 def bob_the_reins
-  put ""
+  puts "1. Holding your arms out in front of you, straight and at chest level"
+  puts "2. Cross your right wrist over your left and hold them together"
+  puts "3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times."
